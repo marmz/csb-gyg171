0b4f02fc4df7fc6377302974deee4c0b47ae6050
Code Sandbox <hello@codesandbox.io>
2022-08-06 05:28:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4c3d5ff7c306cc0689c6efeea498c15f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcd77cba043c2b2a26db2b78454f248",
+    "url": "https://marmz.github.io/csb-gyg171/index.html"
+  },
+  {
+    "revision": "1c5f31349b17cda72efb",
+    "url": "https://marmz.github.io/csb-gyg171/static/css/main.b835f712.chunk.css"
+  },
+  {
+    "revision": "f8cf0377a9698d71aef4",
+    "url": "https://marmz.github.io/csb-gyg171/static/js/2.c6b0e5d5.chunk.js"
+  },
+  {
+    "revision": "1c5f31349b17cda72efb",
+    "url": "https://marmz.github.io/csb-gyg171/static/js/main.6c2e533f.chunk.js"
+  },
+  {
+    "revision": "b7157d684a872687bd48",
+    "url": "https://marmz.github.io/csb-gyg171/static/js/runtime~main.3a1dac6e.js"
+  }
+]);
